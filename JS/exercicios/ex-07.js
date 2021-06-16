9eca5a89465717c0298c409f04b27edb0ece5bbb
AntoniodeNardin <antoniodenardin01@gmail.com>
2021-06-16 15:41:32 -0300

    Initial commit

---
+++ JS/exercicios/ex-07.js
@@ -0,0 +1,7 @@
+var vel = 40
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel>50){
+console.log('você ultrapassou a velocidade permitida. MULTADO')
+}else{
+console.log ('você nao ultrapassou a velocidade permitida.')
+}
---
+++ JS/exercicios/exercicio-09/script.js
@@ -0,0 +1,17 @@
+function carregar() {
+var msg = window.document.getElementById('msg')
+var imagem = window.document.getElementById('img')
+var data =new Date()
+var hora = data.getHours() 
+msg.innerHTML = (`<h2>Agora são ${hora} horas</h2>`)
+if (hora >=6 && hora <= 12) {
+    img.src = 'img-dia.jpg'
+    document.body.style.background = '#F2BE7E'
+}else if (hora >12 && hora <= 18) {
+    img.src = 'img-tarde.jpg'
+    document.body.style.background = '#012626'
+}else {
+    img.src = 'img-noite.jpg'
+    document.body.style.background = '#171C26'
+}
+}
